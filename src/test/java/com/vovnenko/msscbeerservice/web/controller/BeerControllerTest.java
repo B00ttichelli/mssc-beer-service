fbd91331c7ad0aaf2776c7d9dba43d22164f7318
B00ttichelli <donttrytofish@protonmail.com>
2021-09-29 17:41:51 +0300

    Get Beer By Id documented

--- src/test/java/com/vovnenko/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/vovnenko/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,22 +1,38 @@
 package com.vovnenko.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vovnenko.msscbeerservice.domain.Beer;
+import com.vovnenko.msscbeerservice.repositories.BeerRepository;
 import com.vovnenko.msscbeerservice.web.model.BeerDto;
 import com.vovnenko.msscbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 @WebMvcTest(BeerController.class)
+@ComponentScan(basePackages = "com.vovnenko.msscbeerservice.web.mappers")
 class BeerControllerTest {
 
     @Autowired
@@ -24,11 +40,38 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    BeerRepository beerRepository;
+
 
     @Test
     void getBeerById() throws Exception {
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+
+        given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
+
+        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString())
+                        .param("isCold", "yes")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("v1/beer",
+                        pathParameters(
+                                parameterWithName("beerId").description("UUID of desired beer to get.")),
+                        requestParameters(
+                                parameterWithName("isCold").description("Is beer Cold  Query Parameter")
+                        ), responseFields(
+                                fieldWithPath("id").description("Id of Beer"),
+                                fieldWithPath("version").description("version number"),
+                                fieldWithPath("createDate").description("Date of creation"),
+                                fieldWithPath("lastModifiedDate").description("last modification Date"),
+                                fieldWithPath("beerName").description("Name of Beer"),
+                                fieldWithPath("beerStyle").description("Beer Style"),
+                                fieldWithPath("upc").description("Upc Number"),
+                                fieldWithPath("price").description("Price of beer"),
+                                fieldWithPath("quantityOnHand").description("Quantity in stock")
+
+                        )
+
+                ));
     }
 
     @Test
